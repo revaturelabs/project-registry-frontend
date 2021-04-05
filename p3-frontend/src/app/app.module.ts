90615c60670fd9ce3d199951c5468343f4aea3d0
Omar Sancho <omarsancho08@gmail.com>
2021-04-05 15:34:41 -0400

    added table and sort function

--- p3-frontend/src/app/app.module.ts
+++ p3-frontend/src/app/app.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatTableModule} from '@angular/material/table';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ViewProjectsComponent } from './components/view-projects/view-projects.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 @NgModule({
@@ -15,7 +22,15 @@ import { ViewProjectsComponent } from './components/view-projects/view-projects.
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatToolbarModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatFormFieldModule,
+    MatInputModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- p3-frontend/src/app/components/view-projects/view-projects.component.ts
+++ p3-frontend/src/app/components/view-projects/view-projects.component.ts
@@ -1,4 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { ViewChild } from '@angular/core';
+
+interface IPost{
+  id?:string,
+  author?:String,
+  title?:string,
+  category?: string,
+  date?:string
+}
 
 @Component({
   selector: 'app-view-projects',
@@ -7,9 +19,77 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewProjectsComponent implements OnInit {
 
-  constructor() { }
+  dataSource: MatTableDataSource<IPost>;
+  posts: IPost[];
+  displayedColumns:string[]=['id','author','title','category','date'];
 
-  ngOnInit(): void {
+  //sort not fully functional
+  @ViewChild(MatSort) sort: MatSort | any;
+  @ViewChild(MatPaginator) paginator: MatPaginator | any;
+
+  constructor() {
+    this.posts=[{
+      id:'1',
+      author:"John Doe",
+      title:"Testing tabel",
+      category:"fiction",
+      date:"02/23/2021 10:10:10"
+
+    },
+    {
+      id:'2',
+      author:"Jane Doe",
+      title:"Testing tabel2",
+      category:"Sports",
+      date:"02/25/2021 10:10:10"
+
+    },
+    {
+      id:'3',
+      author:"Timmy Turner",
+      title:"Testing tabel2",
+      category:"MMA",
+      date:"02/2/2021 10:10:10"
+
+    },
+    {
+      id:'4',
+      author:"Tim Whatly",
+      title:"Testing tabel2",
+      category:"Music",
+      date:"02/5/2021 10:10:10"
+
+    },
+    {
+      id:'5',
+      author:"Bobby Bob",
+      title:"Testing tabel2",
+      category:"Tech",
+      date:"02/20/2021 10:10:10"
+
+    }]
+
+    this.dataSource=new MatTableDataSource(this.posts)
+   }
+
+   ngOnInit(): void {
+
+
   }
+
+   ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+  // Filter the columns
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
 
 }
