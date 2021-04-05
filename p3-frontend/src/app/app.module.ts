79a73f4ede65a8175cc8d5f9f1c1d27ba1d652f9
xStaples <daltonspencer112@gmail.com>
2021-04-05 08:52:06 -0700

    removed login component

--- p3-frontend/src/app/app.module.ts
+++ p3-frontend/src/app/app.module.ts
@@ -3,14 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './components/login/login.component';
 import { ViewprojectsComponent } from './components/viewprojects/viewprojects.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
     ViewprojectsComponent,
     NavbarComponent
   ],
