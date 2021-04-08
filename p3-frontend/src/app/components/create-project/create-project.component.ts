bf056b5e83928132746132264c979cf0d34d0707
Rodrigo Guerra <avalon_211@hotmail.com>
2021-04-08 11:33:12 -0400

    add tag service and worked on tag chips

--- p3-frontend/src/app/components/create-project/create-project.component.ts
+++ p3-frontend/src/app/components/create-project/create-project.component.ts
@@ -1,10 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Project } from 'src/app/models/project.model';
 import { Role } from 'src/app/models/role.model';
 import { Status } from 'src/app/models/status.model';
 import { Tag } from 'src/app/models/tag.model';
 import { User } from 'src/app/models/user.model';
 import { ProjectService } from 'src/app/service/project.service';
+import { TagService } from 'src/app/service/tag.service';
+import { COMMA, ENTER } from '@angular/cdk/keycodes';
+import {MatAutocompleteSelectedEvent, MatAutocomplete} from '@angular/material/autocomplete';
+
 
 @Component({
   selector: 'app-create-project',
@@ -12,15 +16,24 @@ import { ProjectService } from 'src/app/service/project.service';
   styleUrls: ['./create-project.component.css']
 })
 export class CreateProjectComponent implements OnInit {
+  visible = true;
+  selectable = true;
+  removable = true;
+  separatorKeysCodes:number[] = [ENTER, COMMA];
+  selectedTags:Tag[] = [new Tag(1, "Angular", "This project used an Angular Frontend")];
+  @ViewChild('fruitInput') fruitInput: ElementRef<HTMLInputElement>;
+  @ViewChild('auto') matAutocomplete: MatAutocomplete;
+
   public newProject: Project = new Project(0, "", new Status(1,"IN_ITERATION",""),"", new User(1, "william", new Role(1,"admin")), [new Tag(1, "Revature", "Made by Revature")]);
   public projectName: string = "Name";
   public projectDescription: string = "Description";
+  public tags:Tag[] = [];
 
   public errorDetected:boolean = false;
-  constructor(public projectService: ProjectService) { }
+  constructor(public projectService: ProjectService, public tagService: TagService) {  }
 
   ngOnInit(): void {
-
+    this.getAllTags();
   }
   public registerProject() {
     this.newProject.name = this.projectName;
@@ -44,4 +57,14 @@ export class CreateProjectComponent implements OnInit {
         });
   }
 
+  printTag() {
+    console.log(this.tags);
+  }
+
+  getAllTags(){
+    this.tagService.getAllTags().subscribe(data => {
+      this.tags = data;
+    })
+
+  };
 }
