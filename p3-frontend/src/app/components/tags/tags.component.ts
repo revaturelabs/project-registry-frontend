898a10538e4e19387675ebbd880aa2b086467354
xStaples <daltonspencer112@gmail.com>
2021-04-09 14:11:32 -0700

    fixed status filter options duplicates

--- p3-frontend/src/app/components/tags/tags.component.ts
+++ p3-frontend/src/app/components/tags/tags.component.ts
@@ -1,119 +1,119 @@
-import { MatNativeDateModule } from '@angular/material/core';
-import { Tag } from 'src/app/models/tag.model';
-import { TagService } from './../../service/tag.service';
-import { ProjectService } from './../../service/project.service';
+// import { MatNativeDateModule } from '@angular/material/core';
+// import { Tag } from 'src/app/models/tag.model';
+// // import { TagService } from './../../service/tag.service';
+// import { ProjectService } from './../../service/project.service';
 
-import { COMMA, ENTER } from '@angular/cdk/keycodes';
-import { Component, ElementRef, ViewChild, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
-import { MatAutocompleteSelectedEvent, MatAutocomplete } from '@angular/material/autocomplete';
-import { MatChipInputEvent } from '@angular/material/chips';
-import { Observable } from 'rxjs';
-import { map, startWith } from 'rxjs/operators';
-import { NgbModalConfig, NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
+// import { COMMA, ENTER } from '@angular/cdk/keycodes';
+// import { Component, ElementRef, ViewChild, OnInit } from '@angular/core';
+// import { FormControl } from '@angular/forms';
+// import { MatAutocompleteSelectedEvent, MatAutocomplete } from '@angular/material/autocomplete';
+// import { MatChipInputEvent } from '@angular/material/chips';
+// import { Observable } from 'rxjs';
+// import { map, startWith } from 'rxjs/operators';
+// import { NgbModalConfig, NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 
-@Component({
-  selector: 'app-tags',
-  templateUrl: './tags.component.html',
-  styleUrls: ['./tags.component.css'],
-  providers: [NgbModalConfig, NgbModal]
-})
-export class TagsComponent implements OnInit {
+// @Component({
+//   selector: 'app-tags',
+//   templateUrl: './tags.component.html',
+//   styleUrls: ['./tags.component.css'],
+//   providers: [NgbModalConfig, NgbModal]
+// })
+// export class TagsComponent implements OnInit {
 
-  ngOnInit(): void {
-    this.getAllTags();
-  }
-
-
-  visible = true;
-  selectable = true;
-  removable = true;
-  separatorKeysCodes: number[] = [ENTER, COMMA];
-  tagCtrl = new FormControl();
-  filteredTags: Observable<Tag[]>;
-  selectedTagNames: string[] = [];
-  //store tags of current project, this will be passed to other teams
-  selectedTagArr: Tag[] = [];
-
-  @ViewChild('tagInput')
-  tagInput!: any;
-  @ViewChild('auto')
-  matAutocomplete!: MatAutocomplete;
+//   ngOnInit(): void {
+//     // this.getAllTags();
+//   }
 
 
-  public tagsNames: Tag[] = [];
-  public tags: Tag[] = [];
-  public errorDetected: boolean = false;
+//   visible = true;
+//   selectable = true;
+//   removable = true;
+//   separatorKeysCodes: number[] = [ENTER, COMMA];
+//   tagCtrl = new FormControl();
+//   filteredTags: Observable<Tag[]>;
+//   selectedTagNames: string[] = [];
+//   //store tags of current project, this will be passed to other teams
+//   selectedTagArr: Tag[] = [];
+
+//   @ViewChild('tagInput')
+//   tagInput!: any;
+//   @ViewChild('auto')
+//   matAutocomplete!: MatAutocomplete;
 
 
-  constructor(public projectService: ProjectService, public tagService: TagService, config: NgbModalConfig, private modalService: NgbModal) {
-    config.backdrop = 'static';
-    config.keyboard = false;
-
-    this.filteredTags = this.tagCtrl.valueChanges.pipe(
-      startWith(null),
-      map((tagName: string | null) => tagName ? this._filter(tagName) : this.tagsNames.slice()));
-  }
+//   public tagsNames: Tag[] = [];
+//   public tags: Tag[] = [];
+//   public errorDetected: boolean = false;
 
 
-  closeResult = '';
+//   constructor(public projectService: ProjectService, public tagService: TagService, config: NgbModalConfig, private modalService: NgbModal) {
+//     config.backdrop = 'static';
+//     config.keyboard = false;
 
-  open(content: any) {
-    this.modalService.open(content);
-  }
+//     this.filteredTags = this.tagCtrl.valueChanges.pipe(
+//       startWith(null),
+//       map((tagName: string | null) => tagName ? this._filter(tagName) : this.tagsNames.slice()));
+//   }
 
-  Tag() {
-    console.log(this.tags);
-  }
 
-  getAllTags() {
-    this.tagService.getAllTags().subscribe(data => {
-      this.tags = data;
-      data.forEach(tag => {
-        this.tagsNames.push(tag);
-      })
-    })
-  };
-  private _filter(value: any): Tag[] {
-    // const filterValue = value;
-    const a: Tag = new Tag(0, value, '');
-    return this.tagsNames.filter(tagName => tagName.name == a.name);
-  }
+//   closeResult = '';
 
-  //tagName.indexOf(filterValue) === 0
-  add(event: MatChipInputEvent): void {
-    console.log('add is called')
-    const input = event.input;
-    const value = event.value;
-    console.log('value' + value);
-    if ((value || '').trim()) {
-      if (!this.selectedTagNames.includes(value.trim())) {
-        alert('inside');
-        this.selectedTagNames.push(value.trim());
-      }
-    }
-    if (input) {
-      input.value = '';
-    }
-    this.tagCtrl.setValue(null);
-  }
+//   open(content: any) {
+//     this.modalService.open(content);
+//   }
 
-  remove(tagName: string): void {
-    const index = this.selectedTagNames.indexOf(tagName);
-    if (index >= 0) {
-      this.selectedTagNames.splice(index, 1);
-    }
-  }
+//   Tag() {
+//     console.log(this.tags);
+//   }
 
-  selected(event: MatAutocompleteSelectedEvent): void {
-    let index = this.selectedTagNames.indexOf(event.option.value);
-    if (index == -1) {
-      this.selectedTagNames.push(event.option.viewValue);
-    }
-  }
+//   // getAllTags() {
+//   //   this.tagService.getAllTags().subscribe(data => {
+//   //     this.tags = data;
+//   //     data.forEach(tag => {
+//   //       this.tagsNames.push(tag);
+//   //     })
+//   //   })
+//   // };
+//   private _filter(value: any): Tag[] {
+//     // const filterValue = value;
+//     const a: Tag = new Tag(0, value, '');
+//     return this.tagsNames.filter(tagName => tagName.name == a.name);
+//   }
 
-  //filter out own selected method
-  filterSelectedTag(tag: Tag): void {
-    this.selectedTagArr.push(tag);
-  }
-}
+//   //tagName.indexOf(filterValue) === 0
+//   add(event: MatChipInputEvent): void {
+//     console.log('add is called')
+//     const input = event.input;
+//     const value = event.value;
+//     console.log('value' + value);
+//     if ((value || '').trim()) {
+//       if (!this.selectedTagNames.includes(value.trim())) {
+//         alert('inside');
+//         this.selectedTagNames.push(value.trim());
+//       }
+//     }
+//     if (input) {
+//       input.value = '';
+//     }
+//     this.tagCtrl.setValue(null);
+//   }
+
+//   remove(tagName: string): void {
+//     const index = this.selectedTagNames.indexOf(tagName);
+//     if (index >= 0) {
+//       this.selectedTagNames.splice(index, 1);
+//     }
+//   }
+
+//   selected(event: MatAutocompleteSelectedEvent): void {
+//     let index = this.selectedTagNames.indexOf(event.option.value);
+//     if (index == -1) {
+//       this.selectedTagNames.push(event.option.viewValue);
+//     }
+//   }
+
+//   //filter out own selected method
+//   filterSelectedTag(tag: Tag): void {
+//     this.selectedTagArr.push(tag);
+//   }
+// }
