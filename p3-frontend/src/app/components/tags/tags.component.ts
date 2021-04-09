2203f09502ceff0fde185f14b21eaa6be80c97a7
moghadam117 <saman.moghadam@gmail.com>
2021-04-08 20:16:13 -0700

    Bugs fixed for tag component

--- p3-frontend/src/app/components/tags/tags.component.ts
+++ p3-frontend/src/app/components/tags/tags.component.ts
@@ -1,3 +1,4 @@
+import { MatNativeDateModule } from '@angular/material/core';
 import { Tag } from 'src/app/models/tag.model';
 import { TagService } from './../../service/tag.service';
 import { ProjectService } from './../../service/project.service';
@@ -9,18 +10,20 @@ import {MatAutocompleteSelectedEvent, MatAutocomplete} from '@angular/material/a
 import {MatChipInputEvent} from '@angular/material/chips';
 import {Observable} from 'rxjs';
 import {map, startWith} from 'rxjs/operators';
-import {CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop';
+import { NgbModalConfig, NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-tags',
   templateUrl: './tags.component.html',
-  styleUrls: ['./tags.component.css']
+  styleUrls: ['./tags.component.css'],
+  providers: [NgbModalConfig, NgbModal]
 })
 export class TagsComponent implements OnInit {
   
   ngOnInit(): void {
     this.getAllTags();
   }
+ 
 
   visible = true;
   selectable = true;
@@ -28,9 +31,9 @@ export class TagsComponent implements OnInit {
   separatorKeysCodes:number[] = [ENTER, COMMA];
   tagCtrl = new FormControl();
   filteredTags: Observable<string[]>;
-  selectedTagNames:string[] = ["Angular"];
+  selectedTagNames:string[] = [];
   @ViewChild('tagInput')
-  tagInput!: ElementRef<HTMLInputElement>;
+  tagInput!:any;
   @ViewChild('auto')
   matAutocomplete!: MatAutocomplete;
   
@@ -38,12 +41,20 @@ export class TagsComponent implements OnInit {
   public tagsNames:string[] = [];
   public tags:Tag[] = [];
   public errorDetected:boolean = false;
-  constructor(public projectService: ProjectService, public tagService: TagService) {
+  constructor(public projectService: ProjectService, public tagService: TagService,config: NgbModalConfig, private modalService: NgbModal) {
+    config.backdrop = 'static';
+    config.keyboard = false;
+
     this.filteredTags = this.tagCtrl.valueChanges.pipe(
       startWith(null),
       map((tagName:string|null) => tagName ? this._filter(tagName): this.tagsNames.slice()));
     }
-  printTag() {
+    closeResult = '';
+    open(content:any) {
+      this.modalService.open(content);
+    }
+
+  Tag() {
     console.log(this.tags);
   }
   getAllTags(){
@@ -62,7 +73,9 @@ export class TagsComponent implements OnInit {
     const input = event.input;
     const value = event.value;
     if((value || '').trim()){
+      if (!this.selectedTagNames.includes(value.trim())){
       this.selectedTagNames.push(value.trim());
+    }
     }
     if(input) {
       input.value='';
@@ -76,8 +89,17 @@ export class TagsComponent implements OnInit {
     }
   }
   selected(event: MatAutocompleteSelectedEvent): void {
-    this.selectedTagNames.push(event.option.viewValue);
-    this.tagInput.nativeElement.value='';
-    this.tagCtrl.setValue(null);
+    let index = this.selectedTagNames.indexOf(event.option.value);
+    if(index == -1){
+      this.selectedTagNames.push(event.option.viewValue);
+    }
+
+    
+    
+
+    
+    
+   
+
   }
 }
