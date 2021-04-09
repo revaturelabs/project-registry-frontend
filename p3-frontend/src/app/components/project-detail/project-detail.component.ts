dabc948d9c61fb5cd9c4341b1c596f3072127e9c
Quinn McGreer <quinnmcgreer@gmail.com>
2021-04-09 09:44:22 -0700

    use forms instead of Mat for project-detail

--- p3-frontend/src/app/components/project-detail/project-detail.component.ts
+++ p3-frontend/src/app/components/project-detail/project-detail.component.ts
@@ -2,6 +2,9 @@ import { Project } from 'src/app/models/project.model';
 import { ViewProjectService } from './../../service/view-project.service';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Status } from 'src/app/models/status.model';
+import { User } from 'src/app/models/user.model';
+import { Role } from 'src/app/models/role.model';
 
 
 
@@ -13,6 +16,22 @@ import { ActivatedRoute } from '@angular/router';
 export class ProjectDetailComponent implements OnInit {
 
   constructor(private viewProjectService:ViewProjectService, private router:ActivatedRoute) { }
+
+  //From heroes:
+  powers = ['Really Smart', 'Super Flexible',
+  'Super Hot', 'Weather Changer'];
+
+  statuses = ['ACTIVE', 'NEEDS_ATTENTION', 'ARCHIVED'];
+
+  model = new Project(1, "name", new Status(1, "name", "desc"), "sample desc", new User(1, "username", new Role(1, "string")), []);
+
+  submitted = false;
+
+  onSubmit() { this.submitted = true; }
+
+
+
+
 
   public desiredId:number=1 //this.router.snapshot.params['id'];
   public projects?:Project[]=[]
