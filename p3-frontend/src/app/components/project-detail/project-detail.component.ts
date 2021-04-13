1df384ef96b414f5c3694b733d1f8269941c58a0
Quinn McGreer <quinnmcgreer@gmail.com>
2021-04-13 10:39:34 -0700

    add method to get iteration count

--- p3-frontend/src/app/components/project-detail/project-detail.component.ts
+++ p3-frontend/src/app/components/project-detail/project-detail.component.ts
@@ -5,7 +5,7 @@ import { Project } from 'src/app/models/project.model';
 import { ViewProjectService } from './../../service/view-project.service';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import {Location} from '@angular/common';
+import { Location } from '@angular/common';
 
 import { Status } from 'src/app/models/status.model';
 import { User } from 'src/app/models/user.model';
@@ -23,26 +23,29 @@ import { Iteration } from '../../models/iteration.model';
 })
 export class ProjectDetailComponent implements OnInit {
 
-  constructor(private viewProjectService:ViewProjectService,
-              private iterationService:IterationService,
-              private projectService:ProjectService,
-              private router:ActivatedRoute,
-              private route: Router,
-              private location: Location) { }
+  constructor(private viewProjectService: ViewProjectService,
+    private iterationService: IterationService,
+    private projectService: ProjectService,
+    private router: ActivatedRoute,
+    private route: Router,
+    private location: Location) { }
 
   //In future link to status table?
-  public statusMap:Record<string, number>={
-    IN_ITERATION:1,
-    CODE_FREEZE:2,
-    CODE_REVIEW:3,
-    NEEDS_CLEANUP:4,
-    READY_FOR_ITERATION:5,
-    ACTIVE:6,
-    NEEDS_ATTENTION:7,
-    ARCHIVED:8,
+  public statusMap: Record<string, number> = {
+    IN_ITERATION: 1,
+    CODE_FREEZE: 2,
+    CODE_REVIEW: 3,
+    NEEDS_CLEANUP: 4,
+    READY_FOR_ITERATION: 5,
+    ACTIVE: 6,
+    NEEDS_ATTENTION: 7,
+    ARCHIVED: 8,
   };
 
   public statuses = ['ACTIVE', 'NEEDS_ATTENTION', 'ARCHIVED', 'CODE_REVIEW'];
+
+  public iterationArr: Iteration[] = [];
+  public iterationMap:Map<number, number>= new Map<number, number>();
 
 
   //Temporary model
@@ -51,12 +54,6 @@ export class ProjectDetailComponent implements OnInit {
   submitted = false;
 
   onSubmit() { this.submitted = true; }
-
-  //needed?
-  // submitted = false;
-  // onSubmit() { this.submitted = true; }
-
-
 
   // Group5 Iterator: Passing batch to detail-project
   sendBatch: batchTemplate = {
@@ -68,8 +65,8 @@ export class ProjectDetailComponent implements OnInit {
     endDate: ""
   };
 
-  iteration?: Iteration ;
-  tempIteration?: Iteration ;
+  iteration?: Iteration;
+  tempIteration?: Iteration;
 
 
   iterationToSend: Iteration = {
@@ -85,14 +82,14 @@ export class ProjectDetailComponent implements OnInit {
       },
       description: "",
       owner: {
-      id: 0,
-      username: "",
-      role: {
-        id:0,
-        type: "",
+        id: 0,
+        username: "",
+        role: {
+          id: 0,
+          type: "",
         },
       },
-    tags: [],
+      tags: [],
     },
     startDate: "",
     endDate: ""
@@ -101,7 +98,7 @@ export class ProjectDetailComponent implements OnInit {
 
   // set emit event value to batchIdNum and batchBatchIdStr
   // CHECK CONSOLE FOR ID AND BATCHID
-  changeBatch(value:batchTemplate){
+  changeBatch(value: batchTemplate) {
     this.sendBatch = value;
     console.log(this.sendBatch);
   }
@@ -109,78 +106,99 @@ export class ProjectDetailComponent implements OnInit {
   // -- end Group5 Iterator: Passing batch to view-projec
 
 
-                            //change to this once project is connected
-  public desiredId:number=1 //this.router.snapshot.params['id'];
-  public projects?:Project[]=[]
+  //change to this once project is connected
+  public desiredId: number = 1 //this.router.snapshot.params['id'];
+  public projects: Project[] = []
 
   // Group 5: accidently mess up and forget what it used to be. So we put ?
-  public project?:Project ;
+  public project?: Project;
 
   public selectedId: any = '';
-  
 
   ngOnInit(): void {
 
     this.selectedId = this.router.snapshot.params['id'];
-    
+
 
     //get all projects
-    this.viewProjectService.GetAllProjects().subscribe((data)=> {
-      this.projects=data;
+    this.viewProjectService.GetAllProjects().subscribe((data) => {
+      this.projects = data;
 
       this.project = this.projects.filter(x => {
         return x.id == this.selectedId;
       })[0];
-      
+
       console.log(`Projects: ${this.projects}`);
       console.log(`Selected Project: ${JSON.stringify(this.project)}`);
-    });
-    
-  } 
-  
-  //Update Project in the backend
-  public submit():void{
 
-    // Team5 space
-    //batchId:String, batchProject:Project, id: String, startDate: string, endDate: string
-    if (this.sendBatch && this.project){
-      this.iteration = new Iteration(this.sendBatch.batchId, this.project, this.sendBatch.id, this.sendBatch.startDate, this.sendBatch.endDate);
-      console.log(this.iteration);
-    }
-    // -- End team5 space
+      // find iteration count for each project and store it in iterationMap
+      this.iterationService.getAllIterations().subscribe((data) => {
+        this.iterationArr = data;
+        console.log(JSON.stringify(this.iterationArr));
 
-    //Check that button is connected
 
-    //console.log("submit");
+          this.iterationArr.forEach(element => {
 
-    if (this.project){
-    
-      //Setting the status id
-      this.project.status.id=this.statusMap[this.project.status.name];  
-      console.log(`status sending: ${this.project.status.name}`);
+              if (!this.iterationMap.has(element?.project?.id as number)) {
 
-      //check TS updates
-      //this.project.name="rideForceTest";
+                this.iterationMap.set(element?.project?.id as number, 1);
+              } else {
+                this.iterationMap.set(element?.project?.id as number, this.iterationMap.get(element?.project?.id as number) as number + 1);
+              }
 
-      this.projectService.updateProject(this.project).subscribe((data)=>{
-        this.project=data;
-        console.log(data)
+          })
+
+        console.log(this.iterationMap);
       });
-    }
-    this.route.navigate(['']);
+
+    });
+  }
+
+
+
+  //Update Project in the backend
+  public submit(): void {
+
+  // Team5 space
+  //batchId:String, batchProject:Project, id: String, startDate: string, endDate: string
+  if(this.sendBatch && this.project){
+  this.iteration = new Iteration(this.sendBatch.batchId, this.project, this.sendBatch.id, this.sendBatch.startDate, this.sendBatch.endDate);
+  console.log(this.iteration);
+}
+// -- End team5 space
+
+//Check that button is connected
+
+//console.log("submit");
+
+if (this.project) {
+
+  //Setting the status id
+  this.project.status.id = this.statusMap[this.project.status.name];
+  console.log(`status sending: ${this.project.status.name}`);
+
+  //check TS updates
+  //this.project.name="rideForceTest";
+
+  this.projectService.updateProject(this.project).subscribe((data) => {
+    this.project = data;
+    console.log(data)
+  });
+}
+this.route.navigate(['']);
     //window.location.href = "http://localhost:4200/";
   }
 
-  goBack():void {
-    this.location.back();
-  }
+goBack(): void {
+  this.location.back();
+}
 
-  sendIteration() {
-    console.log("sendIteration() was hit");
-    this.iterationToSend.batchId = this.sendBatch?.batchId;
-    this.iterationToSend.project = this.project;
-    console.log("Here is the iteration we are about to send: " + this.iterationToSend);
-    this.iterationService.sendIteration(this.iterationToSend).subscribe((data: Iteration) => this.tempIteration = data);
+sendIteration() {
+  console.log("sendIteration() was hit");
+  this.iterationToSend.batchId = this.sendBatch?.batchId;
+  this.iterationToSend.project = this.project;
+  console.log("Here is the iteration we are about to send: " + this.iterationToSend);
+  this.iterationService.sendIteration(this.iterationToSend).subscribe((data: Iteration) => this.tempIteration = data);
 }
 
 }
--- p3-frontend/src/app/service/iteration.service.ts
+++ p3-frontend/src/app/service/iteration.service.ts
@@ -1,8 +1,11 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 import { batchTemplate } from 'src/app/models/batch.model';
+import { REGISTRY_URL } from 'src/environments/environment';
 import { Iteration } from '../models/iteration.model';
+
 
 @Injectable({
   providedIn: 'root'
@@ -12,11 +15,27 @@ export class IterationService {
   //url for the API containing batches
   apiUrl = "https://caliber2-mock.revaturelabs.com/mock/training/batch";
 
+  httpOptions = {
+    headers: new HttpHeaders({'Content-Type' : 'application/json'})
+  }
+
   constructor(private http: HttpClient) { }
 
   getBatchService(): Observable<batchTemplate[]>{
     return this.http.get<batchTemplate[]>(this.apiUrl)
   }
+
+  public getAllIterations():Observable<Iteration[]>{
+    return this.http.get<Iteration[]>(`${REGISTRY_URL}iteration`, this.httpOptions)
+    .pipe(
+      catchError(this.handleError<Iteration[]>('getAllIterations',[]))
+    )}
+    private handleError<T>(operation = 'operation', result?: T) {
+      return (error: any): Observable<T> => {
+        console.error(error); // log it to the console if something goes wrong
+        return of(result as T);
+      }
+    } 
 
   sendIteration(iteration: Iteration): Observable<Iteration> {
     console.log("Here is the iteration we're about to send: " + JSON.stringify(iteration));
