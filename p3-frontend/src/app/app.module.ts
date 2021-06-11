d3a4b27e6c95e082e0a3595699ef3cef3317d63b
Ruben <rubengarcia0515@gmail.com>
2021-06-11 09:54:06 -0700

    added base add-tags component

--- p3-frontend/src/app/app.module.ts
+++ p3-frontend/src/app/app.module.ts
@@ -29,6 +29,7 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { TagsComponent } from './components/tags/tags.component';
 import { NavComponent } from './components/nav/nav.component';
 import { ProjectLoginComponent } from './components/project-login/project-login.component';
+import { AddTagsComponent } from './components/tags/add-tags/add-tags.component';
 
 
 
@@ -43,6 +44,7 @@ import { ProjectLoginComponent } from './components/project-login/project-login.
     TagsComponent,
     NavComponent,
     ProjectLoginComponent,
+    AddTagsComponent,
 
 
   ],
