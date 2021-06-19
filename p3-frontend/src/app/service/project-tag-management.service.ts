750361dc4c8b8b8c42aab39414cf13d0adbe318c
Ruben <rubengarcia0515@gmail.com>
2021-06-19 16:28:54 -0700

    base files for service

---
+++ p3-frontend/src/app/service/project-tag-management.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { Tag } from '../models/tag.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProjectTagManagementService {
+
+  private a = new BehaviorSubject<Tag[]>([]);
+  currentA = this.a.asObservable();
+
+  constructor() { }
+
+  updateTagArray(arr: Tag[]){
+    this.a.next(arr);
+  }
+}
