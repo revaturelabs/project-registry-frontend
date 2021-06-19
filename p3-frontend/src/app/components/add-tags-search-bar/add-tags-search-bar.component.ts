9050c125c0384e5f7e9f1299707bd3cffa49cb8c
Ruben <rubengarcia0515@gmail.com>
2021-06-19 15:31:57 -0700

    search-components done

--- p3-frontend/src/app/components/add-tags-search-bar/add-tags-search-bar.component.ts
+++ p3-frontend/src/app/components/add-tags-search-bar/add-tags-search-bar.component.ts
@@ -1,15 +1,76 @@
 import { Component, OnInit } from '@angular/core';
+import {Tag} from "../../models/tag.model"
+import { TagService } from 'src/app/service/tag.service';
+import { Subject } from 'rxjs';
+import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
+import {map, startWith} from 'rxjs/operators'
+import { MatOption } from '@angular/material/core';
+import { element } from 'protractor';
+//import 'rxjs/add/operator/debounceTime'
+//import 'rxjs/add/operator/map'
+//import 'rxjs/add/operator/distinctUntilChanged'
 
 @Component({
   selector: 'app-add-tags-search-bar',
   templateUrl: './add-tags-search-bar.component.html',
   styleUrls: ['./add-tags-search-bar.component.css']
 })
+
 export class AddTagsSearchBarComponent implements OnInit {
+  selectedTag!: Tag;
+allSelectedTags: string[] = [];
+allSelectedTagsObject: Tag[] = [];
+myControl = new FormControl();
+options: string[] = [];
+tags: Tag[] = []
+filteredOptions!: Observable<string[]>;
+  constructor(private TagsService: TagService) {
 
-  constructor() { }
-
-  ngOnInit(): void {
   }
+ngOnInit(): void{
+    this.getTags()
+    this.filteredOptions = this.myControl.valueChanges
+      .pipe(
+        startWith(''), map(value => this._filter(value))
+      )
+  }
+
+  //gets all tags from service and calls getTagnames function
+  getTags(){
+    this.TagsService.getAllTags().subscribe(tag => {
+      this.tags = tag
+      console.log(this.tags)
+      this.getTagNames(this.tags)
+      console.log(this.options)
+    })
+  }
+  //extracts the name property from tags object and pushes into array of strings
+  getTagNames(arr: any){
+    for(let i=0; i<arr.length; i++){
+     //console.log(arr[i])
+      this.options.push(arr[i].name)
+    }
+  }
+
+ private _filter(value: string): string[]{
+   const filterValue = value.toLowerCase()
+   return this.options.filter(option => option.toLowerCase().includes(filterValue))
+ }
+
+ //this gets the value of the selected tag, option.value only fires when a valid tag is selected, this will get array index of the object we will push for further processing
+ onTagSelected(option: MatOption){
+  console.log(this.myControl)
+  console.log(option.value)
+  const tagName = (element: any) => element.name === option.value;
+  console.log(this.tags.findIndex(tagName))
+  let index = this.tags.findIndex(tagName)
+  if(!this.allSelectedTagsObject.find(tagName)){
+      this.allSelectedTagsObject.push(this.tags[index])
+  }
+  console.log(this.allSelectedTagsObject)
+
+
+ }
 
 }
